f62e62e8400cce2c4b3f86a8ca2fd93670e12a17
Fernando Blanco <fern.alej.blanco@gmail.com>
2019-01-07 01:20:04 -0700

    Overriding GetLetterGrade Method in RankedGradeBook Class
    Overridden the GetLetterGrade Method and added some logic.
    Returns a char that is the letter grade for the student.
    
    It throws an InvalidOperationsException if there are less than 5 students.
    
    A bunch of comparisions are made based on the threshold and grades variables within the method.
    
    Returns 'F' character if all other comparisons failed.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -11,5 +12,40 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            //Lambda Expression instead of foreach loop
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if(grades[(threshold*2)-1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if(grades[(threshold*3)-1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if(grades[(threshold*4)-1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+            
+        }
+
     }
 }
